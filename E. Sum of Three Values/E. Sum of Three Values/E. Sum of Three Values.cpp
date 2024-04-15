f6bf04b54d667cfa828e37aa2e6d4bd410686881
Ryan <heyuanboryan814@outlook.com>
2024-04-15 15:30:22 -0500

    Done with sum3

--- E. Sum of Three Values/E. Sum of Three Values/E. Sum of Three Values.cpp
+++ E. Sum of Three Values/E. Sum of Three Values/E. Sum of Three Values.cpp
@@ -1,6 +1,6 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
-
+typedef pair<int, int>pii;
 
 
 
@@ -8,25 +8,35 @@ int main()
 {
     int n, x;
     cin >> n >> x;
-    vector<int>lst(n);
-    unordered_map<int, int>mp;
+    //vector<int>lst(n);
+    vector<pii>vp(n);
     for (int i = 0; i < n; i++) {
-        cin >> lst[i];
-        mp[lst[i]] = i + 1;
+        pii p;
+        cin >> p.first;
+        p.second = i + 1;
+        vp[i] = p;
     }
-    
 
-    for (int i=0;i<n-2;i++)
+    sort(vp.begin(), vp.end());
+
+    for (int i = 0; i < n; i++)
     {
-	    for (int j=i+1;j<n;j++)
-	    {
-            int want = x - lst[i] - lst[j];
-            if (mp.find(want)!=mp.end())
-            {
-                cout << i + 1 << " " << j + 1 << " " << mp[want] << endl;
+        int want = x - vp[i].first;
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            if (right != i && left != i && vp[left].first + vp[right].first == want) {
+                cout << vp[i].second << " " << vp[left].second << " " << vp[right].second << endl;
                 return 0;
             }
-	    }
+            else if (vp[left].first + vp[right].first < want) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
     }
     cout << "IMPOSSIBLE" << endl;
     return 0;
