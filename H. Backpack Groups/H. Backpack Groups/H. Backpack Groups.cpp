ab64855f64d0be5795f73e715c89870b40106978
Ryan <heyuanboryan814@outlook.com>
2024-04-15 12:56:03 -0500

    unfinished

---
+++ H. Backpack Groups/H. Backpack Groups/H. Backpack Groups.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+
+typedef pair<int, int> pii;
+using namespace std;
+
+
+
+int main() {
+	int v, n, t;
+	cin >> v >> n >> t;
+
+	vector<pii>stuff(n);
+	for (int i = 0; i < n; i++) {
+		int w, c, p;
+		cin >> w >> c >> p;
+
+	}
+}
